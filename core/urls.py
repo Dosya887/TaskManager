1c302a71d6f8542b4035ce150b428f1dfce542f2
Dosya887 <tdastan.777@mail.ru>
2025-12-24 11:02:37 +0600

    Rename project API endpoints to plural form for REST consistency

--- core/urls.py
+++ core/urls.py
@@ -5,11 +5,12 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/user/', include('user.urls')),
-    path('api/v1/project/', include('project.urls')),
-    # path('api/v1/project-member/', include('project_member.urls')),
+    path('api/v1/users/', include('user.urls')),
+    path('api/v1/projects/', include('project.urls')),
+    path('api/v1/project-members/', include('project_member.urls')),
     path('api/v1/tasks/', include('task.urls')),
-    # path('api/v1/comment/', include('comment.urls')),
+    path('api/v1/comments/', include('comment.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += [path('', include('core.swagger_urls'))]
